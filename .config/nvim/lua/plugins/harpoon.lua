7f792c18ac6fe2ac3c30f48664a1c12cdad0b499
Bayley Mauger <bayley@prezzee.com>
2024-06-24 09:32:56 +1000

    Adding harpoon and ufo

---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,10 @@
+-- harpoon.nvim
+-- https://github.com/ThePrimeagen/harpoon/tree/harpoon2
+
+return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+}
---
+++ .config/nvim/lua/plugins/ufo.lua
@@ -0,0 +1,7 @@
+-- nvim-ufo
+-- https://github.com/kevinhwang91/nvim-ufo
+
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+}
