1c0ff8dc65b8643eca743c7c37b42889721edc29
Bayley Mauger <bayley@prezzee.com>
2025-04-23 17:32:41 +1000

    Fix keymaps for oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -24,6 +24,25 @@ return {
 				view_options = {
 					show_hidden = true,
 				},
+				keymaps = {
+					["g?"] = { "actions.show_help", mode = "n" },
+					["<CR>"] = "actions.select",
+					["<C-s>"] = false,
+					["<C-h>"] = false,
+					["<C-t>"] = false,
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = { "actions.close", mode = "n" },
+					["<C-r>"] = "actions.refresh",
+					["-"] = { "actions.parent", mode = "n" },
+					["_"] = { "actions.open_cwd", mode = "n" },
+					["`"] = { "actions.cd", mode = "n" },
+					["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+					["gs"] = { "actions.change_sort", mode = "n" },
+					["gx"] = "actions.open_external",
+					["g."] = { "actions.toggle_hidden", mode = "n" },
+					["g\\"] = { "actions.toggle_trash", mode = "n" },
+				},
+				use_default_keymaps = false,
 			})
 			vim.api.nvim_set_keymap("n", "\\", ':lua require("oil").open()<CR>', { noremap = true, silent = true })
 		end,
