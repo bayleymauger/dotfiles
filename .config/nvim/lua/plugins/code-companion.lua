c74b3e904e3e3bb86d058ae1e4f23374e37d4697
Bayley Mauger <bayley@prezzee.com>
2025-07-24 09:59:18 +1000

    Add keymaps to codecompanion

--- .config/nvim/lua/plugins/code-companion.lua
+++ .config/nvim/lua/plugins/code-companion.lua
@@ -1,8 +1,32 @@
 return {
 	"olimorris/codecompanion.nvim",
-	opts = {},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 	},
+	config = function()
+		local codecompanion = require("codecompanion")
+
+		codecompanion.setup({
+			display = {
+				chat = {
+					window = {
+						width = 0.3,
+					},
+				},
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+		vim.keymap.set(
+			{ "n", "v" },
+			"<LocalLeader>c",
+			"<cmd>CodeCompanionChat Toggle<cr>",
+			{ noremap = true, silent = true }
+		)
+		vim.keymap.set("v", "ca", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+		-- Expand 'cc' into 'CodeCompanion' in the command line
+		vim.cmd([[cab cc CodeCompanion]])
+	end,
 }
