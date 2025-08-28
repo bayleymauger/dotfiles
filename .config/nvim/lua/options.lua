068baf3afa08a13a5234bfa62b1c1550ae053706
Bayley Mauger <bayley@prezzee.com>
2025-08-28 11:22:44 +1000

    Update

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -70,3 +70,6 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- Swap files are not useful for me
+vim.o.swapfile = false
