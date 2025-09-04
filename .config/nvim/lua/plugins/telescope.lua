7110141f6b7eeed60aef7eb43d664c0e6f77958c
Bayley Mauger <bayley@prezzee.com>
2025-09-04 10:44:53 +1000

    Update deps and allow hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -62,12 +62,24 @@ return {
 			-- You can put your default mappings / updates / etc. in here
 			--  All the info you're looking for is in `:help telescope.setup()`
 			--
-			-- defaults = {
-			--   mappings = {
-			--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-			--   },
-			-- },
-			-- pickers = {}
+			defaults = {
+				file_ignore_patterns = { ".git/", "node_modules/" },
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
 			extensions = {
 				["ui-select"] = {
 					require("telescope.themes").get_dropdown(),
