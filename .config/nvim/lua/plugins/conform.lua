6dc26afb25f371e33ced418b0caf5b4c06db6ca4
Bayley Mauger <bayley@prezzee.com>
2025-11-13 14:13:41 +1100

    fix crush mcp, add conform disable and update deps

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -22,7 +22,10 @@ return {
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
 			local disable_filetypes = { c = true, cpp = true }
-			if disable_filetypes[vim.bo[bufnr].filetype] then
+
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return nil
+			elseif disable_filetypes[vim.bo[bufnr].filetype] then
 				return nil
 			else
 				return {
@@ -43,4 +46,27 @@ return {
 			terraform = { "terraform_fmt", "terraform_fmt", stop_after_first = true },
 		},
 	},
+	config = function(_, opts)
+		require("conform").setup(opts)
+
+		-- User commands to disable/enable autoformat
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+	end,
 }
